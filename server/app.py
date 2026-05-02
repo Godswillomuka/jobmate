add4977cae017d8eb9e73158c2fa48f0854782ba
Godswill omuka <omukagodswil@gmail.com>
2026-05-02 13:46:04 +0300

    initialize backend with Flask, PostgreSQL, Flask-Migrate, and routes setup

--- server/app.py
+++ server/app.py
@@ -1,19 +1,22 @@
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-import os
 from dotenv import load_dotenv
+import os
+import models
+
+from extensions import db, migrate
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-# DB config
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
+# INIT EXTENSIONS
+db.init_app(app)
+migrate.init_app(app, db)
 
 @app.route("/")
 def home():
